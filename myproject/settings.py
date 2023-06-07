fbc0f02f112acd27ab8667b1b6fcab99299cb61d
User <dossovmarlen85@gmail.com>
2023-06-07 13:10:03 +0300

    deleted drf-spectacular

--- myproject/settings.py
+++ myproject/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "drf_spectacular",
     "rest_framework",
     "rest_framework_simplejwt",
     "ads",
@@ -147,7 +146,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 AUTH_USER_MODEL = 'users.User'
